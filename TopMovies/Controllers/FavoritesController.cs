8f83af41d6308fa23cdc18f14b0729d49ff6fd4c
Betül Ölçer <betulkok.0@gmail.com>
2022-05-26 10:53:38 +0300

    Add to fav-Remove to fav

---
+++ TopMovies/Controllers/FavoritesController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TopMovies.DTO_s;
+using TopMovies.Models;
+
+namespace TopMovies.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FavoritesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<int> favs = GetFavList();
+            List<Movie> movies = _db.Movies
+                .Where(x => favs.Contains(x.Id))
+                .ToList();
+            return View(movies);
+
+        }
+        [HttpPost]
+        public IActionResult Toggle( int movieId)
+        {
+            List<int> favs = GetFavList();
+            bool fav;
+            if (favs.Contains(movieId))
+            {
+                favs.Remove(movieId);
+                fav = false;
+            }
+            else
+            {
+                favs.Add(movieId);
+                fav= true;
+            }
+            SaveFavList(favs);
+            return Json( new FavoriteToggleResult{ Favorited = fav });
+        }
+
+        private void SaveFavList(List<int> favorites)
+        {
+            string favs = string.Join('-', favorites);
+            HttpContext.Response.Cookies.Append("favs",favs,new CookieOptions() {Expires = DateTime.Now.AddYears(10) });
+        }
+
+        private List<int> GetFavList()
+        {
+            var favs = HttpContext.Request.Cookies["favs"];
+            if (string.IsNullOrEmpty(favs))
+            {
+                return new List<int>();
+            }
+            try
+            {
+                return favs.Split('-').Select(s => Convert.ToInt32(s)).ToList();
+
+            }
+            catch (Exception)
+            {
+
+                return new List<int>();
+            }
+        }
+    }
+}
--- TopMovies/wwwroot/js/site.js
+++ TopMovies/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+
+function success(message) {
+    Toast.fire({
+        icon: 'success',
+        title: message
+    });
+}
