df575cc96fd116a14928485dcfe910c196d9ace1
Betül Ölçer <betulkok.0@gmail.com>
2022-05-26 14:33:55 +0300

    .

--- TopMovies/Controllers/FavoritesController.cs
+++ TopMovies/Controllers/FavoritesController.cs
@@ -6,20 +6,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using TopMovies.DTO_s;
 using TopMovies.Models;
+using TopMovies.Services;
 
 namespace TopMovies.Controllers
 {
     public class FavoritesController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly FavoriteServices _favoriteServices;
 
-        public FavoritesController(ApplicationDbContext db)
+        public FavoritesController(ApplicationDbContext db, FavoriteServices favoriteServices)
         {
             _db = db;
+            _favoriteServices = favoriteServices;
         }
         public IActionResult Index()
         {
-            List<int> favs = GetFavList();
+            List<int> favs = _favoriteServices.GetFavList();
             List<Movie> movies = _db.Movies
                 .Where(x => favs.Contains(x.Id))
                 .ToList();
@@ -29,7 +32,7 @@ namespace TopMovies.Controllers
         [HttpPost]
         public IActionResult Toggle( int movieId)
         {
-            List<int> favs = GetFavList();
+            List<int> favs = _favoriteServices.GetFavList();
             bool fav;
             if (favs.Contains(movieId))
             {
@@ -41,33 +44,10 @@ namespace TopMovies.Controllers
                 favs.Add(movieId);
                 fav= true;
             }
-            SaveFavList(favs);
+            _favoriteServices.SaveFavList(favs);
             return Json( new FavoriteToggleResult{ Favorited = fav });
         }
 
-        private void SaveFavList(List<int> favorites)
-        {
-            string favs = string.Join('-', favorites);
-            HttpContext.Response.Cookies.Append("favs",favs,new CookieOptions() {Expires = DateTime.Now.AddYears(10) });
-        }
-
-        private List<int> GetFavList()
-        {
-            var favs = HttpContext.Request.Cookies["favs"];
-            if (string.IsNullOrEmpty(favs))
-            {
-                return new List<int>();
-            }
-            try
-            {
-                return favs.Split('-').Select(s => Convert.ToInt32(s)).ToList();
-
-            }
-            catch (Exception)
-            {
-
-                return new List<int>();
-            }
-        }
+        
     }
 }
---
+++ TopMovies/Services/FavoriteServices.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopMovies.Services
+{
+    public class FavoriteServices
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FavoriteServices(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public List<int> GetFavList()
+        {
+            var favs = _httpContextAccessor.HttpContext.Request.Cookies["favs"];
+            if (string.IsNullOrEmpty(favs))
+            {
+                return new List<int>();
+            }
+            try
+            {
+                return favs.Split('-').Select(s => Convert.ToInt32(s)).ToList();
+
+            }
+            catch (Exception)
+            {
+
+                return new List<int>();
+            }
+        }
+        public void SaveFavList(List<int> favorites)
+        {
+            string favs = string.Join('-', favorites);
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("favs", favs, new CookieOptions() { Expires = DateTime.Now.AddYears(10) });
+        }
+
+    }
+}
